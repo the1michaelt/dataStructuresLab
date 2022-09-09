c37a6f795ec539e538458883da0e602fc206c03b
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-09 07:24:46 -0400

    fix: task 1, linked list works

--- linked_list.py
+++ linked_list.py
@@ -10,8 +10,7 @@ class LinkedList:
         self.tail = None
 
     def append_node(self, value_to_add):
-        new_node = Node(value_to_add)
-
+        new_node = Node(value_to_add)        
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -19,20 +18,16 @@ class LinkedList:
         else:
             self.tail.next = new_node
             self.tail = self.tail.next
-        value_to_find = new_node
-    
-
+   
+     
     def find_node(self, value_to_find):
-        # while current_node.next is not None: #28 minutes into the video
-        #     current_node = current_node.next  # move node by node
-        while current_node.next is not None: #28 minutes into the video
-        # # and find the node that has the value_to_find
-        # # if a node has it, return True
-        # # if no node has it, return False
+        current_node = self.head.value #Starting point 
+        while current_node is not None: # while there is another node; 28 minutes into the video
+        # move node by node. As long as next is not empty
         # # self.head.value
-        # value_inquiry = value
-            if value_to_find == self.head.value:
-                return True
+            if value_to_find != self.head.value: #returns true on any value that is not the head
+                return True  # # if a node has it, return True
             else:
-                return False
+                return False # # if no node has it, return False
+        current_node = current_node.next
 
